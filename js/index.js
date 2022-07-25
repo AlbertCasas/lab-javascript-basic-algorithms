42c3ba30308fb7c9311b73b1bc329f776bca2649
Albert Casas <albertcasas3d@gmail.com>
2022-07-25 09:53:13 +0200

    adding loops

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
 //
-let hacker1 = "";
+let hacker1 = prompt("What is the driver's name?");
 console.log(`The driver's name is ${hacker1}`)
-let hacker2 = "";
+let hacker2 = ("What is the navigator's name?");
 console.log(`The navigator's name is ${hacker2}`)
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length){
@@ -13,4 +13,18 @@ if (hacker1.length > hacker2.length){
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
 }
 
-// Iteration 3: Loops
+hackerUpper = hacker1.toUpperCase
+hackerUpperSpaced = ""
+space = " "
+for (i = 0; i < hackerUpper.length; i++) {
+    console.log(hackerUpperSpaced += hackerUpper[i] + space)
+}
+
+
+let reverseHacker2 = ""
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    console.log(reverseHacker2 += hacker2[i]);  
+}
+    
+
